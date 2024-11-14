8ace985ba6225ba7f45300f8fec475f61507afa5
Robertamotta <72874002+Robertamotta@users.noreply.github.com>
2024-11-14 20:53:21 -0300

    Criação teste unitário

---
+++ EmissorCartoes.Testes/Dominio/Servico/ProcessadorEmissaoCartaoServicoTeste.cs
@@ -0,0 +1,41 @@
+﻿using EmissorCartoes.Dominio.DTOs;
+using EmissorCartoes.Dominio.Interfaces;
+using EmissorCartoes.Dominio.Servico;
+using Moq;
+
+namespace EmissorCartoes.Testes.Dominio.Servico
+{
+    public class ProcessadorEmissaoCartaoServicoTeste
+    {
+        private readonly Mock<IMensageria> mensageriaMock = new();
+
+        private readonly ProcessadorEmissaoCartaoServico processadorEmissaoCartaoServico ;
+
+        public ProcessadorEmissaoCartaoServicoTeste()
+        {
+            processadorEmissaoCartaoServico = new ProcessadorEmissaoCartaoServico(mensageriaMock.Object);
+        }
+
+        [Theory]
+        [InlineData(1300, 1)]
+        [InlineData(3000, 2)]
+        [InlineData(6000, 3)]
+        public async Task Processar_DeveEnviarEmissaoCartoesComQuantidadeCorreta(decimal renda, int qtdEsperada)
+        {
+            // Arrange
+            var propostaCredito = new PropostaCredito
+            {
+                Id = 1,
+                Renda = renda
+            };
+
+            mensageriaMock.Setup(m => m.EnviarEmissaoCartoes(It.IsAny<EmissaoCartaoCreditoDto>())).Returns(Task.CompletedTask);
+
+            // Act
+            await processadorEmissaoCartaoServico.Processar(propostaCredito);
+
+            // Assert
+            mensageriaMock.Verify(m => m.EnviarEmissaoCartoes(It.Is<EmissaoCartaoCreditoDto>(x => x.Id == propostaCredito.Id && x.QtdCartoesEmitidos == qtdEsperada)), Times.Once);
+        }
+    }
+}
